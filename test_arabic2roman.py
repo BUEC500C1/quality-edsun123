742152ee9e5925cc2fc915ce731196f678b1c8ca
edsun123 <34005715+edsun123@users.noreply.github.com>
2020-04-24 16:42:38 -0400

    Update test_arabic2roman.py
    changed to error check for inputs

--- test_arabic2roman.py
+++ test_arabic2roman.py
@@ -10,6 +10,10 @@ if __name__ == "__main__":
     app.run()
     
 def arabic2roman(num):
+    if(isinstance(x,int)!=True):
+        print("bad input")
+        return false
+    
     dec = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     rom = ["M", "CM", "D", "CD", "C", "XC", "L", "XL","X", "IX", "V", "IV","I"]
     result=''
